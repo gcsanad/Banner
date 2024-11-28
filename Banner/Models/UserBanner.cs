8ebcbc35a68e9ca7dcacbea456aac2b5b9e19fb5
Csanad <you@example.com>
2024-11-28 14:09:59 +0100

    Add project files.

---
+++ Banner/Models/UserBanner.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banner.Models
+{
+    internal class UserBanner : ABanner, IBannerOperations
+    {
+        public void Clear()
+        {
+            Fill(BackgroundColor);
+        }
+
+        public void DrawLine(int lineIndex, Color drawingColor, IBannerOperations.Orientation lineOrientation)
+        {
+            if (lineOrientation == IBannerOperations.Orientation.Horizontally && lineIndex <= RowNum && lineIndex >= 0)
+            {
+                
+                for (int columnIndex = 0; columnIndex < ColumnNum; columnIndex++)
+                {
+                    this[lineIndex, columnIndex] = drawingColor;
+                }
+            }
+            else if(lineOrientation == IBannerOperations.Orientation.Vertically && lineIndex <= ColumnNum && lineIndex >= 0)
+            {
+                for (int rowIndex = 0; rowIndex < RowNum; rowIndex++)
+                {
+                    this[rowIndex, lineIndex] = drawingColor;
+                }
+            }
+        }
+
+        public void Fill(Color fillColor)
+        {
+            for (int rowIndex = 0; rowIndex < RowNum; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < ColumnNum; columnIndex++)
+                {
+                    this[rowIndex, columnIndex] = fillColor;
+                    
+                }
+            }
+        }
+
+        public void ReplaceColor(Color oldColor, Color newColor)
+        {
+            for (int rowIndex = 0; rowIndex < RowNum; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < ColumnNum; columnIndex++)
+                {
+                    if (this[rowIndex, columnIndex] == oldColor)
+                    {
+                        this[rowIndex, columnIndex] = newColor;
+                    }
+                }
+            }
+        }
+
+        public void RotateToLeft()
+        {
+            for (int rowIndex = 0; rowIndex < RowNum; rowIndex++)
+            {
+                Color tempColor = this[rowIndex, ColumnNum + 1];
+                for (int columnIndex = 0; columnIndex < ColumnNum; columnIndex++)
+                {
+                    this[rowIndex, columnIndex] = this[rowIndex, columnIndex + 1];
+                }
+                this[rowIndex, 0] = tempColor;
+            }
+        }
+
+        public void RotateToRight()
+        {
+            for (int rowIndex = 0; rowIndex < RowNum; rowIndex++)
+            {
+                Color tempColor = this[rowIndex, ColumnNum - 1];
+                for (int columnIndex = 0; columnIndex < ColumnNum; columnIndex++)
+                {
+                    this[rowIndex, columnIndex] = this[rowIndex,columnIndex-1];
+                }
+                this[rowIndex, 0]= tempColor;
+            }
+        }
+
+        public void ShiftToLeft(Color fillColor)
+        {
+            for (int rowIndex = 0; rowIndex < RowNum; rowIndex++)
+            {
+                Color tempColor = this[rowIndex, ColumnNum + 1];
+                for (int columnIndex = 0; columnIndex < ColumnNum; columnIndex++)
+                {
+                    if (columnIndex == ColumnNum)
+                    {
+                        this[rowIndex, columnIndex] = fillColor;
+                    }
+                    else
+                    {
+                        this[rowIndex, columnIndex] = this[rowIndex, columnIndex + 1];
+
+                    }
+                }
+                this[rowIndex, 0] = tempColor;
+            }
+        }
+
+        public void ShiftToRight(Color fillColor)
+        {
+            for (int rowIndex = 0; rowIndex < RowNum; rowIndex++)
+            {
+                Color tempColor = this[rowIndex, ColumnNum - 1];
+                for (int columnIndex = 0; columnIndex < ColumnNum; columnIndex++)
+                {
+                    if (columnIndex == 0)
+                    {
+                        this[rowIndex, columnIndex] = fillColor;
+                    }
+                    else
+                    {
+                        this[rowIndex, columnIndex] = this[rowIndex, columnIndex + 1];
+
+                    }
+                }
+                this[rowIndex, 0] = tempColor;
+            }
+        }
+    }
+}
---
+++ Banner/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace Banner
+{
+    internal class Program
+    {
+        enum Days { Hétfő, Kedd, Szerda, Vasárnap }
+        static void Main(string[] args)
+        {
+
+            if (Days.Kedd < Days.Vasárnap) { }
+
+            for (Days nap = Days.Hétfő; nap <= Days.Szerda; nap++)
+            {
+                switch (nap)
+                {
+                    case Days.Hétfő:
+
+                        break;
+                    case Days.Kedd:
+
+                        break;
+                    case Days.Szerda:
+                        break;
+                    case Days.Vasárnap:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write('■');
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine('■');
+
+        }
+    }
+}
